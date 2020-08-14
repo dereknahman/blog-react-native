72ea83a3a66964f4fa0ff0b0df4969c06195ee5b
dereknahman <44810431+dereknahman@users.noreply.github.com>
2020-08-14 16:29:14 +0100

    created reducer and context

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import { Context } from "../context/BlogContext";
+
+const IndexScreen = () => {
+  const { state, addBlogPost } = useContext(Context);
+
+  return (
+    <View style={styles.container}>
+      <Text>Index Screen</Text>
+      <Button title="Add post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+});
+
+export default IndexScreen;
